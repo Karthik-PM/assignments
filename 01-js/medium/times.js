9b5b7b06b36d85f5542bf751f1856a6ac852b62d
Karthik-PM <karthikpm52@gmail.com>
2024-08-22 11:44:12 +0530

    added time.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -1,5 +1,6 @@
 /*
-Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
+Write a function that calculates the time (in seconds) it
+takes for the JS code to calculate sum from 1 to n, given n as the input.
 Try running it for
 1. Sum from 1-100
 2. Sum from 1-100000
@@ -7,7 +8,14 @@ Try running it for
 Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
-
 function calculateTime(n) {
-    return 0.01;
+    let time = 0; 
+    let sum = 0;
+    const t1 = Date.now();
+    for(let i = 0; i < n; i++){
+        sum += i;
+    }
+    const t2 = Date.now();
+    console.log("time taken : " + (t2 - t1) / 1000) + "Minutes";
 }
+calculateTime(10000000);
