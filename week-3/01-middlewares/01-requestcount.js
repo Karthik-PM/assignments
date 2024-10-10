fcd774cf399cc418da419df47d52a3725f08cd8e
Karthik-PM <karthikpm52@gmail.com>
2024-10-10 12:14:00 +0530

    added middlewares/01-requestCount.json

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -9,6 +9,13 @@ let requestCount = 0;
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+function countRequest(req, res, next){
+    requestCount++;
+    console.log(requestCount);
+    next();
+}
+
+app.use(countRequest);
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -22,4 +29,5 @@ app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
 
+app.listen(3000);
 module.exports = app;
